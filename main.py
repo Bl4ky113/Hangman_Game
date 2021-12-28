e7f0f43331dfa527281334dc9dc88a9b0cced734
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-12-28 15:49:20 -0500

    1rst commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Made By Bl4ky113
+
+import tkinter as tk
+
+def main (): 
+    base_tk = tk.Tk(className="Hangman The Game", screenName="Hangman The Game", baseName="Hangman The Game")
+    base_tk.geometry("1000x1000")
+
+    base_tk.mainloop()
+if __name__ == "__main__":
+    main()
