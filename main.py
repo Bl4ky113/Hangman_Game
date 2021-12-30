21b74817a690a2c33af06b94c7c77dc61a6ed981
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-12-30 17:46:01 -0500

    Added Entry Input

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import tkinter as tk
 from tk_classes import tkinter_text, tkinter_wrapper, tkinter_input
 import os 
 
+# Tkinter items creators.
+text_creator = tkinter_text()
+inputs_creator = tkinter_input() 
 
-def main (): 
+def main ():
+    # Display The Main Page
     base_tk = tk.Tk(className="Hangman The Game")
+
+
+    entry_variable = tk.StringVar()
 
     # Calc the size of the base_tk. 3/4 of the user screen on with and height
     screen_width = base_tk.winfo_screenwidth()
@@ -23,10 +30,6 @@ def main ():
         pady=20
     )
 
-    # Tkinter items creators.
-    text_creator = tkinter_text()
-    inputs_creator = tkinter_input()
- 
     # Header and Bl4ky113
     header_wrapper = tkinter_wrapper(base_tk, (0, "x", "top"))
     text_creator.title(header_wrapper, ("both", "left"), "Hangman")
@@ -37,6 +40,12 @@ def main ():
 
     # Main 
     main_wrapper = tkinter_wrapper(base_tk, (1, "both", "top"))
+    # Word to Guess and Hangman Icon
+    content_wrapper = tkinter_wrapper(main_wrapper, (1, "both", "top"))
+    text_creator.word_to_guess(content_wrapper, ("both", "left"), "H E L L O")
+    # Text Input
+    input_wrapper = tkinter_wrapper(base_tk, (1, "x", "top"))
+    inputs_creator.txt_input(input_wrapper, entry_variable, ("both", "top", "center"))
 
     # Footer
     footer_wrapper = tkinter_wrapper(base_tk, (0, "x", "bottom"))
--- tk_classes.py
+++ tk_classes.py
@@ -3,7 +3,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-bg_color = "#333"
+bg_color = "#333333"
 fg_color = "#e9e9e9"
 
 def tkinter_wrapper (tk_element, pack_options=()):
@@ -21,7 +21,7 @@ def tkinter_wrapper (tk_element, pack_options=()):
 
 class tkinter_text ():
     def title (self, tk_element, pack_options=(), content=""):
-        title_bg_color = f"#{int(bg_color[1:]) + 111}"
+        title_bg_color = f"#{int(bg_color[1:]) + 111111}"
 
         title_element = ttk.Label(
             tk_element,
@@ -53,6 +53,24 @@ class tkinter_text ():
             side=pack_options[1],
         )
 
+    def word_to_guess (self, tk_element, pack_options=(), content=""):
+        word_bg_color = f"#{hex(int(bg_color[1:], 16) + int('151515', 16))[2:]}"
+        
+        text_element = ttk.Label(
+            tk_element,
+            padding=(20, 10),
+            font=("Arial Black", 18, "underline"),
+            foreground=fg_color,
+            background=word_bg_color,
+            text=content,
+            anchor="center"
+        )
+        text_element.pack(
+            expand=1,
+            fill=pack_options[0],
+            side=pack_options[1]
+        )
+
     def bl4ky (self, tk_element, pack_options=()):
         text_element = ttk.Label(
             tk_element,
@@ -71,21 +89,17 @@ class tkinter_text ():
 
 class tkinter_input ():
     def btn_input (self, tk_element, onclick, content="", pack_options=""):
-        input_color = f"#{int(bg_color[1:]) + 111}"
-        active_input_color = f"#{int(bg_color[1:]) + 222}"
+        input_color = f"#{int(bg_color[1:]) + 111111}"
+        active_input_color = f"#{int(bg_color[1:]) + 222222}"
 
         btn = tk.Button(
             tk_element,
-
             font=("Arial Greek", 14),
             text=content,
             command=onclick,
-
             border=0,
-
             background=input_color,
             foreground=fg_color,
-            
             activebackground=active_input_color,
             activeforeground=fg_color,
         )
@@ -93,4 +107,30 @@ class tkinter_input ():
             ipadx=5,
             padx=5,
             side=pack_options
+        )
+
+    def txt_input (self, tk_element, output_variable, pack_options=()):
+        input_color = f"#{int(bg_color[1:]) - 111111}"
+        active_input_color = f"#{int(bg_color[1:]) - 111111}"
+
+        text_input = tk.Entry(
+            tk_element,
+            textvariable= output_variable,
+            justify="center",
+            border=0,
+            font=("Arial Black", 20),
+            foreground=fg_color,
+            background=input_color,
+            highlightbackground=active_input_color,
+            insertbackground=fg_color,
+            insertwidth=5,
+        )
+        text_input.pack(
+            ipadx=10,
+            ipady=10,
+            padx=5,
+            pady=5,
+            expand=1,
+            side=pack_options[1],
+            anchor=pack_options[2]
         )
